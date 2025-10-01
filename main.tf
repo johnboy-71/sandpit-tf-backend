b60247ae799a098cf9b68e4e9bc4fadad2942b12
John Fagan <johnfagan@Johns-MacBook-Air.local>
2025-10-01 14:23:06 +0100

    ec2 update

--- main.tf
+++ main.tf
@@ -18,3 +18,42 @@ provider "aws" {
   region              = var.aws.region
   allowed_account_ids = var.aws_account_id
 }
+
+data "aws_ami" "test_latest_windows" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-English-Full-Base-*"]
+  }
+}
+
+resource "aws_instance" "my_ec2_server" {
+  ami           = data.aws_ami.test_latest_windows.id
+  instance_type = "t2.small" # Specify an instance type
+  subnet_id     = "subnet-044071109ad3de8ea"
+
+
+
+  tags = {
+    Name  = devwinserver01
+    Owner = "J_Fagan"
+  }
+}
+
+# module "ec2_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = ""
+
+#   instance_type = "t3.micro"
+#   key_name      = "user1"
+#   monitoring    = true
+#   subnet_id     = "subnet-eddcdzz4"
+
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
--- vars.tf
+++
@@ -1,3 +0,0 @@
-aws_region     = "eu-west-1a"
-aws_account_id = ["731219982456"]
-environment    = "sandpit"
